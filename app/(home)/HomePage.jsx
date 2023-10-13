c83990d435fb36ddd78793a69b2051b97456d8a8
MgHtetMyatAung <htetmyataung16000@gmail.com>
2023-10-13 21:37:02 +0630

    change-style

--- app/(home)/HomePage.jsx
+++ app/(home)/HomePage.jsx
@@ -17,7 +17,7 @@ export default async function HomePage() {
   return (
     <section>
       <div className="container mx-auto">
-        <h1 className=" text-xl text-center font-semibold mb-5">
+        <h1 className=" text-xl text-center font-semibold my-5">
           Latest Articles
         </h1>
         <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-5">
